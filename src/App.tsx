6c11926b1a330d9082111b357e056e1b93bb987d
Arpit Shukla <arpitnshukla@gmail.com>
2022-03-27 18:57:03 +0200

    Initial testing with BNB in BSC testnet.

---
+++ src/App.tsx
@@ -0,0 +1,68 @@
+import './App.css';
+import { useMoralis, useWeb3ExecuteFunction } from "react-moralis";
+import {Moralis} from 'moralis';
+
+function App() {
+
+   
+  const { authenticate, isAuthenticated, isAuthenticating, user, account, logout, chainId } = useMoralis();
+
+  const login = async () => {
+    if (!isAuthenticated) {
+
+      await authenticate({signingMessage: "Log in to the Website" })
+        .then(function (user) {
+          console.log("logged in user:", user);
+          console.log(user!.get("ethAddress"));
+          console.log("chain id: ", chainId);
+        })
+        .catch(function (error) {
+          console.log(error);
+        });
+        var chain = await Moralis.getChainId();
+        console.log(chain !== null ? parseInt(chain) : chain )
+    }
+  }
+
+  const logOut = async () => {
+    await logout();
+    console.log("logged out");
+  }
+
+  const payBnb = async () => {
+    if (!isAuthenticated) {
+      login()
+    }
+    var chain = await Moralis.getChainId();
+    var chainId;
+    if ( chain!=null)
+    {
+      chainId = parseInt(chain)
+    }
+    console.log("null check done ", chainId)
+    if (chainId != 97){
+      console.log("chainID check done ", chainId)
+      alert("Please connect to BNB testnet.")
+      return;
+    }
+    let result = await Moralis.transfer({
+      type: "native",
+      amount: Moralis.Units.ETH("0.0001"),
+      receiver: "0xB760800B21Feb733278978b0582Bc04f62cc0FD3"
+    });
+    await console.log("executed transfer")
+    await console.log(result)
+  }
+
+  return (
+    <div>
+      <h1>Park Sale Live!! </h1><br/>
+      <label>Amount: </label>
+      <button onClick={login}>Metamask Login</button>
+      <button onClick={logOut} disabled={isAuthenticating}>Logout</button>
+      <button onClick={payBnb}>Pay</button>
+    </div>
+  );
+}
+
+export default App;
